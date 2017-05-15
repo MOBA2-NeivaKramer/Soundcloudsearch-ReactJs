cb11ac5bbab6569b6539c9965e4936e4a4483949
Lucas Neiva <lucasneiv4@gmail.com>
2017-05-15 13:20:52 +0200

    Improve station board row rendering

--- native/app/stationboard.js
+++ native/app/stationboard.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react';
 import {AppRegistry, StyleSheet, View, ListView, Text, TouchableHighlight} from 'react-native';
 import api from 'api';
+import Moment from 'moment';
 
 export default class Stationboard extends Component {
     static navigationOptions = {
@@ -10,16 +11,23 @@ export default class Stationboard extends Component {
 
     constructor(props) {
         super(props);
-        const { params } = props.navigation.state;
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
             dataSource: ds.cloneWithRows([])
         };
+    }
 
-        api.getStationboard(params.location.id).then(result => {
-            console.log('Stationboard', result)
-            this.setState({dataSource: ds.cloneWithRows(result.stationboard)});
-        })
+    componentWillMount() {
+        const { params } = this.props.navigation.state;
+        api.getStationboard(params.location.id)
+            .then(result => {
+                console.log('Stationboard', result)
+                Stationboard.navigationOptions.title = result.station.name + 'Stationboard';
+                this.setState({dataSource: this.state.dataSource.cloneWithRows(result.stationboard)});
+            })
+            .catch(error => {
+                throw error
+            })
     }
 
     render() {
@@ -34,8 +42,9 @@ export default class Stationboard extends Component {
 
     renderRow(rowData: string, sectionID: number, rowID: number, highlightRow: (sectionID: number, rowID: number) => void) {
         return <TouchableHighlight>
-            <View style={{height: '30px'}}>
-                <Text>{rowData.name}</Text>
+            <View style={{padding: 10}}>
+                <Text style={{color: 'black'}}>{Moment(rowData.stop.departure).fromNow()}</Text>
+                <Text>{rowData.name + ' to ' + rowData.to}</Text>
             </View>
         </TouchableHighlight>
     }
